7a82e6728aea1af5287492eae0ced8743d55f6e7
shmuelmb <shmuel.cohen0@gmail.com>
2024-10-14 12:12:55 +0300

    update

--- callback.js
+++ callback.js
@@ -1,3 +1,103 @@
+// Type JavaScript here and click "Run Code" or press Ctrl + s
+// console.log('Hello, world!');
+
+// Challenge 1
+function addTwo(num) {
+  return num + 2;
+}
+
+// To check if you've completed it, uncomment these console.logs!
+// console.log(addTwo(3));
+// console.log(addTwo(10));
+
+// Challenge 2
+function addS(word) {
+  return word + "s";
+}
+
+// uncomment these to check your work
+// console.log(addS('pizza'));
+// console.log(addS('bagel'));
+
+// Challenge 3
+function map(array, callback) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(callback(array[i]));
+  }
+  return result;
+}
+
+// console.log(map([1,2,3,4,5], (num)=> num*2));
+
+// Challenge 4
+function forEach(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i]);
+  }
+}
+
+// let alphabet = '';
+// const letters = ['a', 'b', 'c', 'd'];
+// forEach(letters, function(char) {
+//   alphabet += char;
+// });
+// console.log(alphabet);
+
+// Challenge 5
+function mapWith(array, callback) {
+  const result = [];
+  forEach(array, (item) => result.push(callback(item)));
+  return result;
+}
+// console.log(mapWith([1,2,3,4,5], (num)=> num*2));
+
+// Challenge 6
+function reduce(arr, callback, initial) {
+  let result = initial;
+  for (let i = 0; i < arr.length; i++) {
+    result = callback(result, arr[i]);
+  }
+  return result;
+}
+// const nums = [4, 1, 3];
+// const add = function (a, b) {
+//   return a + b;
+// };
+// console.log(reduce(nums, add, 0));
+
+// Challenge 7
+function intersection(arrays) {
+  return arrays.reduce((acc, curr) => {
+    return curr.filter((el) => acc.includes(el));
+  });
+}
+
+// console.log(
+//   intersection([
+//     [5, 10, 15, 20],
+//     [15, 88, 1, 5, 7],
+//     [1, 10, 15, 5, 20],
+//   ])
+// );
+// should log: [5, 15]
+
+// Challenge 8
+function union(arrays) {
+  return arrays.reduce((acc, curr) => {
+    const elements = curr.filter((el) => !acc.includes(el));
+    return acc.concat(elements);
+  });
+}
+console.log(
+  union([
+    [5, 10, 15],
+    [15, 88, 1, 5, 7],
+    [100, 15, 10, 1, 5],
+  ])
+);
+// should log: [5, 10, 15, 88, 1, 7, 100]
+
 // Challenge 9
 function objOfMatches(array1, array2, callback) {
   let result = {};
